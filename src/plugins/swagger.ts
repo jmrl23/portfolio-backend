a8f486e6c08d33267fea6ae16d6cf1f0d6e36df8
Jomariel Gaitera <gaiterajomariel@gmail.com>
2024-08-08 17:46:00 +0800

    feat(api): sync api version from package.json

--- src/plugins/swagger.ts
+++ src/plugins/swagger.ts
@@ -3,6 +3,8 @@ import fastifySwaggerUi from '@fastify/swagger-ui';
 import fastifyPlugin from 'fastify-plugin';
 import type { OpenAPIV3_1 } from 'openapi-types';
 import { PRODUCTION_URL } from '../lib/constant/env';
+import fs from 'node:fs';
+import path from 'node:path';
 
 export default fastifyPlugin(async function swagger(app) {
   const servers: OpenAPIV3_1.ServerObject[] = [
@@ -19,13 +21,18 @@ export default fastifyPlugin(async function swagger(app) {
     });
   }
 
+  const packageJson = fs
+    .readFileSync(path.resolve(__dirname, '../../package.json'))
+    .toString();
+  const version: string = JSON.parse(packageJson)?.version ?? '0.0.0';
+
   await app.register(fastifySwagger, {
     prefix: '/docs',
     openapi: {
       openapi: '3.1.0',
       info: {
         title: 'Rest API',
-        version: '0.0.1',
+        version,
       },
       servers,
       components: {
