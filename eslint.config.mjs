14a5ac1fda4eb0d909cd10f639926ec7f169d4c7
Jomariel Gaitera <gaiterajomariel@gmail.com>
2024-08-28 20:28:21 +0800

    fix: eslint bug

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default [
-  { files: ['src/**/*.{js,ts}'] },
   {
     languageOptions: {
       globals: { ...globals.node },
