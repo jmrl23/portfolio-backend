aae4dd75b90c4b08aae7bf36da27a6e79e9294cd
Jomariel Gaitera <gaiterajomariel@gmail.com>
2024-08-28 18:07:00 +0800

    feat(swagger): Add production server URL

--- src/lib/constant/env.ts
+++ src/lib/constant/env.ts
@@ -19,7 +19,9 @@ export const IMAGEKIT_URL_ENDPOINT = env
 
 export const REDIS_URL = env.get('REDIS_URL').required().asString();
 
-export const PRODUCTION_URL = env.get('PRODUCTION_URL').asUrlString();
+export const PRODUCTION_URL =
+  env.get('PRODUCTION_URL').asUrlString() ??
+  env.get('VERCEL_PROJECT_PRODUCTION_URL').asString();
 
 export const AUTH_KEY = env.get('AUTH_KEY').required().asString();
 
--- src/plugins/swagger.ts
+++ src/plugins/swagger.ts
@@ -4,6 +4,7 @@ import fastifyPlugin from 'fastify-plugin';
 import fs from 'node:fs';
 import path from 'node:path';
 import { OpenAPIV3_1 } from 'openapi-types';
+import { PRODUCTION_URL } from '../lib/constant/env';
 
 export default fastifyPlugin(async function (app) {
   const packageJson: Record<string, unknown> = JSON.parse(
@@ -18,6 +19,18 @@ export default fastifyPlugin(async function (app) {
       description: 'Default local development server',
     },
   ];
+
+  if (PRODUCTION_URL !== undefined) {
+    let url = PRODUCTION_URL;
+    if (!url.startsWith('http')) {
+      url = 'https://' + url;
+    }
+
+    servers.unshift({
+      url,
+      description: 'Production server URL',
+    });
+  }
 
   await app.register(fastifySwagger, {
     prefix: '/docs',
