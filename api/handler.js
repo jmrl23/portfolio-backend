05509bb8782583037f14a3b76ce21b9465c48c07
Jomariel Gaitera <gaiterajomariel@gmail.com>
2024-08-28 17:57:43 +0800

    feat: add vercel edge function handler

---
+++ api/handler.js
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const fastify = require('fastify');
+const app = fastify();
+/** @type {import('fastify-plugin').default} */
+const bootstrap = require('../build/bootstrap');
+
+app.register(bootstrap);
+
+/**
+ * @param {import('node:http').IncomingMessage} request
+ * @param {import('node:http').ServerResponse} response
+ * */
+async function handler(request, response) {
+  await app.ready();
+  app.server.emit('request', request, response);
+}
+
+module.exports = handler;
