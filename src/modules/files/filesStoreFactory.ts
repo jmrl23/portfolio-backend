3ba63c51085729158b84cd39da59feb7a3b93a19
Jomariel Gaitera <gaiterajomariel@gmail.com>
2024-09-18 15:27:28 +0800

    refactor(api): isolate store related things

--- src/modules/files/filesStoreFactory.ts
+++ src/modules/files/filesStoreFactory.ts
@@ -1,11 +1,5 @@
-import ImageKit from 'imagekit';
 import { ReadStream } from 'node:fs';
-import {
-  IMAGEKIT_PRIVATE_KEY,
-  IMAGEKIT_PUBLIC_KEY,
-  IMAGEKIT_URL_ENDPOINT,
-} from '../../lib/constant/env';
-import { ImagekitStore } from './stores/imagekitStore';
+import { ImageKitStore } from './stores/imagekitStore';
 
 export type FileData = string | Buffer | ReadStream;
 
@@ -27,22 +21,6 @@ type Store = 'imagekit';
 export async function filesStoreFactory(store: Store): Promise<FilesStore> {
   switch (store) {
     case 'imagekit':
-      const requiredOptions = [
-        IMAGEKIT_PUBLIC_KEY,
-        IMAGEKIT_PRIVATE_KEY,
-        IMAGEKIT_URL_ENDPOINT,
-      ];
-
-      if (requiredOptions.includes(undefined)) {
-        throw new Error('Missing imagekit required option');
-      }
-
-      const imagekit = new ImageKit({
-        publicKey: IMAGEKIT_PUBLIC_KEY!,
-        privateKey: IMAGEKIT_PRIVATE_KEY!,
-        urlEndpoint: IMAGEKIT_URL_ENDPOINT!,
-      });
-
-      return new ImagekitStore(imagekit);
+      return new ImageKitStore();
   }
 }
--- src/modules/files/stores/imagekitStore.ts
+++ src/modules/files/stores/imagekitStore.ts
@@ -1,10 +1,30 @@
 import ImageKit from 'imagekit';
 import { lookup } from 'mime-types';
-import { IMAGEKIT_FOLDER } from '../../../lib/constant/env';
+import {
+  IMAGEKIT_FOLDER,
+  IMAGEKIT_PRIVATE_KEY,
+  IMAGEKIT_PUBLIC_KEY,
+  IMAGEKIT_URL_ENDPOINT,
+} from '../../../lib/constant/env';
 import { FileData, FileInfo, FilesStore } from '../filesStoreFactory';
 
-export class ImagekitStore implements FilesStore {
-  constructor(private readonly imagekit: ImageKit) {}
+export class ImageKitStore implements FilesStore {
+  private imagekit: ImageKit;
+
+  constructor() {
+    const requiredOptions = [
+      IMAGEKIT_PUBLIC_KEY,
+      IMAGEKIT_PRIVATE_KEY,
+      IMAGEKIT_URL_ENDPOINT,
+    ];
+    if (requiredOptions.includes(undefined))
+      throw new Error('Missing required option');
+    this.imagekit = new ImageKit({
+      publicKey: IMAGEKIT_PUBLIC_KEY!,
+      privateKey: IMAGEKIT_PRIVATE_KEY!,
+      urlEndpoint: IMAGEKIT_URL_ENDPOINT!,
+    });
+  }
 
   async upload(fileData: FileData, fileName: string): Promise<FileInfo> {
     const response = await this.imagekit.upload({
